e97d31c7ac3a29b6366399a656d0132cda853cc7
Elizabeth Adegbaju <adeotunadegbaju@gmail.com>
2021-02-28 21:08:16 +0100

    feat: add IsEnabled field to CBAUser

---
+++ CBA/Data/Migrations/20210228193446_User-IsEnabled.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CBA.Data.Migrations
+{
+    public partial class UserIsEnabled : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEnabled",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsEnabled",
+                table: "AspNetUsers");
+        }
+    }
+}
