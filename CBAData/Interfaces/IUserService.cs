328b8090bd85decc3a5318e0f676bdd099b9d666
Elizabeth Adegbaju <adeotunadegbaju@gmail.com>
2021-03-06 00:08:55 +0100

    Update IUserService.cs

--- CBAData/Interfaces/IUserService.cs
+++ CBAData/Interfaces/IUserService.cs
@@ -10,13 +10,21 @@ namespace CBAData.Interfaces
     public interface IUserService
     {
         Task<ManageUserRolesViewModel> ListUserRolesAsync(string userId);
+
         void EditUserRolesAsync(string userId, IList<UserRolesViewModel> userRolesViewModels, CBAUser currentUser);
+
         Task<List<CBAUser>> ListUsersExceptSpecifiedUserAsync(CBAUser user);
+
         ManageUserRolesViewModel LoadEmptyUser();
+
         Task<CBAUser> CreateUserAsync(CBAUser modelUser, string password);
+
         Task UpdateUserRolesAsync(CBAUser user, List<string> userRoles);
+
         void SendAccountConfirmationEmail(string pathToFile, string callbackUrl, CBAUser user, string password);
+
         Task DeleteUserAsync(string userId);
+
         Task UpdateUserAsync(string id, CBAUser user);
     }
 }
