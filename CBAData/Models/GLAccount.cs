13e02573a5d5568793aa00ed0f8d764d2c226f5f
Elizabeth Adegbaju <adeotunadegbaju@gmail.com>
2021-04-02 14:30:28 +0100

    feat: view GL Accounts

--- CBAData/Models/GLAccount.cs
+++ CBAData/Models/GLAccount.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace CBAData.Models
 {
     public class GLAccount
     {
+        public GLAccount()
+        {
+            IsActivated = true;
+        }
+
         public int Id { get; set; }
         public GLCategory Category { get; set; }
+
+        [DisplayName("Account Number")]
         public string AccountNumber { get; set; }
+
+        [DisplayName("Account Name")]
         public string AccountName { get; set; }
         public CBAUser User { get; set; }
+
+        [DisplayName("Account Status")]
         public bool IsActivated { get; set; }
     }
 }
---
+++ CBAWeb/Areas/GL/Controllers/AccountsController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CBAData;
+using CBAData.Models;
+
+namespace CBAWeb.Areas.GL.Controllers
+{
+    [Area("GL")]
+    public class AccountsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AccountsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Accounts
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.GLAccounts.ToListAsync());
+        }
+
+        // GET: Accounts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gLAccount = await _context.GLAccounts
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (gLAccount == null)
+            {
+                return NotFound();
+            }
+
+            return View(gLAccount);
+        }
+
+        // GET: Accounts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Accounts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,AccountNumber,AccountName,IsActivated")] GLAccount gLAccount)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(gLAccount);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(gLAccount);
+        }
+
+        // GET: Accounts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gLAccount = await _context.GLAccounts.FindAsync(id);
+            if (gLAccount == null)
+            {
+                return NotFound();
+            }
+            return View(gLAccount);
+        }
+
+        // POST: Accounts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,AccountNumber,AccountName,IsActivated")] GLAccount gLAccount)
+        {
+            if (id != gLAccount.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(gLAccount);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GLAccountExists(gLAccount.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(gLAccount);
+        }
+
+        // GET: Accounts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gLAccount = await _context.GLAccounts
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (gLAccount == null)
+            {
+                return NotFound();
+            }
+
+            return View(gLAccount);
+        }
+
+        // POST: Accounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var gLAccount = await _context.GLAccounts.FindAsync(id);
+            _context.GLAccounts.Remove(gLAccount);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool GLAccountExists(int id)
+        {
+            return _context.GLAccounts.Any(e => e.Id == id);
+        }
+    }
+}
