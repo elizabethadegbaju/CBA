4744eac661c96086213ae5ff97facfcb73a61750
Elizabeth Adegbaju <adeotunadegbaju@gmail.com>
2021-04-08 18:16:21 +0100

    Update Customer.cs

--- CBAData/Models/Customer.cs
+++ CBAData/Models/Customer.cs
@@ -1,14 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace CBAData.Models
 {
     public class Customer
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
         public long CustomerId { get; set; }
+
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [DisplayName("Email")]
+        public string Email { get; set; }
+
+        [DisplayName("Phone Number")]
+        public string Phone { get; set; }
+
+        [DisplayName("Status")]
         public bool IsActivated { get; set; }
         public List<CustomerAccount> Accounts { get; set; }
     }
