c8b790e1137f3ce1731d1ab8e4d143c7dd93b5fc
Elizabeth Adeotun Adegbaju <adeotunadegbaju@gmail.com>
2021-04-13 11:44:14 +0100

    fix: required columns in GLCategory

--- CBAData/Migrations/20210413102515_glcategory-required-fields.Designer.cs
+++ CBAData/Migrations/20210413102515_glcategory-required-fields.Designer.cs
@@ -4,14 +4,16 @@ using CBAData;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CBAData.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210413102515_glcategory-required-fields")]
+    partial class glcategoryrequiredfields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
