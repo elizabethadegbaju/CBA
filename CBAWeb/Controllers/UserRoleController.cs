4ad185e85c7f68d2dc2b1d639a198c6d21077ac7
Elizabeth Adegbaju <adeotunadegbaju@gmail.com>
2021-05-13 18:21:42 +0100

    Update UserRoleController.cs

--- CBAWeb/Controllers/UserRoleController.cs
+++ CBAWeb/Controllers/UserRoleController.cs
@@ -1,12 +1,6 @@
-﻿using CBAData.Models;
-using CBAData.Interfaces;
+﻿using CBAData.Interfaces;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using CBAData.ViewModels;
 
@@ -14,16 +8,10 @@ namespace CBAWeb.Controllers
 {
     public class UserRoleController : Controller
     {
-        private readonly SignInManager<CBAUser> _signInManager;
-        private readonly RoleManager<CBARole> _roleManager;
-        private readonly UserManager<CBAUser> _userManager;
         private readonly IUserService _userService;
 
-        public UserRoleController(SignInManager<CBAUser> signInManager, RoleManager<CBARole> roleManager, UserManager<CBAUser> userManager, IUserService userService)
+        public UserRoleController(IUserService userService)
         {
-            _signInManager = signInManager;
-            _roleManager = roleManager;
-            _userManager = userManager;
             _userService = userService;
         }
 
@@ -35,39 +23,6 @@ namespace CBAWeb.Controllers
             return View(model);
         }
 
-        // GET: UserRolesController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: UserRolesController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: UserRolesController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: UserRolesController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
         // POST: UserRolesController/Edit/5
         [Authorize(Policy = "CBA002")]
         [HttpPost]
@@ -76,27 +31,6 @@ namespace CBAWeb.Controllers
         {
             await _userService.EditUserRoleAsync(model);
             return RedirectToAction(nameof(UsersController.Index), "Users");
-        }
-
-        // GET: UserRolesController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: UserRolesController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
